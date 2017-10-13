68f6139a44e6b9ab448cfaa13bb83d6f05e1b4a6
Clayton Leach <zeerus@gmail.com>
2017-10-13 16:00:40 -0600

    Trying without dirname

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/', function(req, res, next) {
 });
 
 app.get('/todo-app', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '/public/todo/index.html'));
+  res.sendFile(path.join('/public/todo/index.html'));
 });
 
 // catch 404 and forward to error handler
