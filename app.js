9c8173bda8cb2eda1a361754543e8a01d6682e11
Clayton Leach <Clayton.Leach@outlook.com>
2017-10-13 12:44:57 -0600

    Temporarily move routes into main express app

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var nunjucks = require('nunjucks');
 
-var index = require('./routes/index');
-var todo = require('./routes/todo');
-
 var app = express();
 
 nunjucks.configure('views', {
@@ -24,8 +21,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/todo', todo)
+app.use('/', function(req, res, next) {
+  res.render('index.html');
+});
+
+app.use('/todo', function(req, res, next) {
+  res.sendFile(path.join(__dirname + '/public/todo/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
