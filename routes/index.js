1fc2b1abb95dc1a7426591d747601e9b34cab046
Clayton Leach <Clayton.Leach@outlook.com>
2017-10-13 12:32:18 -0600

    Added possible dependency to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var nunjucks = require('nunjucks');
+
+nunjucks.configure('views', {autoescape: true});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
