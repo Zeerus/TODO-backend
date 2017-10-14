366372d8d432029fb9520bb79c23d301631a207b
Clayton Leach <zeerus@gmail.com>
2017-10-14 10:26:57 -0600

    Chaging order of routes

--- app.js
+++ app.js
@@ -29,20 +29,19 @@ app.get('/', function(req, res, next) {
 });
 
 app.use('/todo/', express.static(path.join(__dirname, 'todo')))
+app.get('/todo', function(req, res, next) {
+    res.sendFile('/todo/index.html', {root: __dirname});
+});
 app.route('/todo/api')
     .get(function(req, res, next) {
         res.send('This will work');
     })
     .post(function(req, res, next) {
-        
+
     })
     .put(function(req, res, next) {
 
     });
-
-app.get('/todo', function(req, res, next) {
-    res.sendFile('/todo/index.html', {root: __dirname});
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
