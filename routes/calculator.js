51a4560ba74b195803378237e64f4d7eccbb7fc3
Clayton Leach <Clayton.Leach@outlook.com>
2017-10-17 12:46:44 -0600

    Slight tweaks

--- routes/calculator.js
+++ routes/calculator.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var path = require('path');
 
 router.get('/', function(req, res, next) {
-    console.log(__dirname);
     res.sendFile('index.html', { root: path.join(__dirname, '../calculator') });
 });
 
